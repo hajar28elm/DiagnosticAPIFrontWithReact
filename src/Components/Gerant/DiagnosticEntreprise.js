cd08a0f137a38975af08b157c9a099330bf31806
youness serrakhi <120403490+YounessSerrakhi@users.noreply.github.com>
2023-10-16 16:20:47 +0100

    set up APIS

--- src/Components/Gerant/DiagnosticEntreprise.js
+++ src/Components/Gerant/DiagnosticEntreprise.js
@@ -1,63 +1,60 @@
-import { Dashboard } from "../Admin/Dashboard"
-import { useState,useEffect } from "react";
-// eslint-disable-next-line no-unused-vars
+import { Dashboard } from "../Admin/Dashboard";
+import { useState, useEffect } from "react";
 import axios from "axios";
-export function DiagnosticEntreprise(){
-    // eslint-disable-next-line no-unused-vars
-    const [userId, setUserId] = useState(null);
-    const [categoryWithScores,setCategoryWithScores] = useState([]);
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-      //const user = fetchUserId();
-    //    const user=1;
-    //   if (user) {
-    //     setUserId(user.id);
-    //     axios.post("/user/diagnostics", { userId: user.id })
-    //       .then((response) => {
-    //         const data = response.data;
-    //         setCategoryWithScores(data);
-    //         console.log("retrieved data", data);
-    //       })
-    //       .catch((error) => {
-    //         console.error("Error sending user ID:", error);
-    //       });
-    //   }
-      setTimeout(() => {
-        const fetchedData = [
-          { pourcentage: "10", category: 'service de production'},
-          { pourcentage: "60", category: 'ressource humaine' },
-          { pourcentage: "70", category: 'finance' },
-        ];
-  
-        setCategoryWithScores(fetchedData);
-        setLoading(false);
-      }, 2000);
-      
-    }, []);
-     return (
-        <> 
-           
-            <Dashboard lien1="questionnaire" lien2="diagnosticEntreprise"/>
-            <div className="container mt-5" style={{marginLeft:"150px"}}>
-             <div className="row">
-             {loading ? (
-               <p>Loading...</p>
-            ) : (
-                <>
-               {categoryWithScores.map((item) => (
-               <div key={item.category} className="card border-primary mb-3" style={{ width: "18rem", marginRight: "20px" }}>
-                   <div className="card-header" >{item.category}</div>
-                   <div className="card-body text-primary">
-                   <h5 className="card-title">Score: {item.pourcentage} %</h5>
-                   <p className="card-text">Some additional information about this category.</p>
+export function DiagnosticEntreprise() {
+  const [categoryWithScores, setCategoryWithScores] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch user diagnostics and scores from your API
+    const user = 1; // Replace with the actual user or fetch it as needed
+
+    if (user) {
+      axios
+        .get("http://127.0.0.1:8000/api/user/diagnostics", { userId: user.id })
+        .then((response) => {
+          const data = response.data.data;
+          setCategoryWithScores(data);
+          setLoading(false);
+          console.log("Retrieved data", data);
+        })
+        .catch((error) => {
+          console.error("Error fetching user diagnostics:", error);
+        });
+    }
+  }, []);
+
+  return (
+    <>
+      <Dashboard lien1="questionnaire" lien2="diagnosticEntreprise" />
+      <div className="container mt-5" style={{ marginLeft: "150px" }}>
+        <div className="row">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              {Object.keys(categoryWithScores).map((categoryId) => {
+                const category = categoryWithScores[categoryId];
+
+                return (
+                  <div
+                    key={categoryId}
+                    className="card border-primary mb-3"
+                    style={{ width: "18rem", marginRight: "20px" }}
+                  >
+                    <div className="card-header">{category.category}</div>
+                    <div className="card-body text-primary">
+                      <h5 className="card-title">Score: {category.percentage} %</h5>
+                      <p className="card-text">Some additional information about this category.</p>
+                    </div>
                   </div>
-               </div>
-              ))}
-              </>
-             )}
-            </div>
-            </div>
-        </>
-     )
+                );
+              })}
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Components/Gerant/Questionnaire.js
+++ src/Components/Gerant/Questionnaire.js
@@ -1,108 +1,111 @@
 import { Dashboard } from "../Admin/Dashboard";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from 'axios';
-export function Questionnaire(){
-    const [data, setData] = useState([]);
-    const [response,setResponse] = useState([]);
-    const [loading, setLoading] = useState(true);
-    // eslint-disable-next-line no-unused-vars
-    const [userResponses, setUserResponses] = useState([]);
-  
-    useEffect(() => {
-      setTimeout(() => {
-        const fetchedData = [
-          { id: 1 , category:"category 1", question: "Question 1", reponses:["je suis d'accord","je desaccord"]},
-          { id:2 , category: "category 2",question: "Question 2", reponses:["je suis d'accord","je desaccord"]},
-        ];
-  
-        setData(fetchedData);
-        setLoading(false);
-      }, 2000);
-      axios.get("/getQuestions")
-      .then((response) => {
-        const fetchedData = response.data;
-        setData(fetchedData);
-        setLoading(false);
+
+export function Questionnaire() {
+  const [data, setData] = useState([]);
+  const [response, setResponse] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [userResponses, setUserResponses] = useState({});
+
+  useEffect(() => {
+    // Fetch questions and responses from your API
+    axios.get("http://127.0.0.1:8000/api/questions")
+      .then((questionsResponse) => {
+        const fetchedQuestions = questionsResponse.data.data;
+        setData(fetchedQuestions);
       })
       .catch((error) => {
         console.error("Error fetching questions:", error);
       });
-      axios.get("/getResponses")
-      .then((response) => {
-        const fetchedResponse = response.data;
-        setResponse(fetchedResponse);
+
+    axios.get("http://127.0.0.1:8000/api/responses")
+      .then((responsesResponse) => {
+        const fetchedResponses = responsesResponse.data.data;
+        setResponse(fetchedResponses);
         setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching responses:", error);
       });
-      
-    }, []);
-      
-       const handleSubmitResponse  = () =>{
-        const userResponsesTab = data.map((question) => ({
-          questionId: question.id,
-          responseId: response.find((res) => res.description === userResponses[question.id]),
-          userId: "USER_ID_HERE",
-        }));
-        axios.post("/submitResponses", userResponsesTab)
-          .then((response) => {
-            console.log("Responses submitted successfully", response);
-          })
-          .catch((error) => {
-            console.error("Error submitting responses:", error);
-          });
-       }
-    return (
-        <div>
-        <Dashboard lien1="questionnaire" lien2="diagnosticEntreprise"/>
-        <div className="container mt-5">
-      <div className="row">
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <table className="table">
-             <thead>
-                <tr >
-                <th colSpan="3" style={{color:"white",fontSize:"30px",backgroundColor:"#033C4D"}}>Question table</th>
-              </tr>
-            </thead>
-            <thead>
-              <tr>
-                <th style={{width:"500px"}}>Category</th>
-                <th style={{width:"500px"}}>Question</th>
-                <th>Response</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((item) => (
-                <tr key={item.id}>
-                  <td>{item.category}</td>
-                  <td>{item.question}</td>
-                  <td>
-                    {/* <select style={{width:"500px"}} onChange={(e) => { setUserResponses({...userResponses,[item.id]: e.target.value,});}} >
-                        {
-                            response.map((res) =>{
-                                return <option key={res.id} value={res.description}>{res.description}</option>;
-                            })
-                        }
-                    </select> */}
-                    <select style={{width:"500px"}}>
-                    {
-                            item.reponses.map((res) =>{
-                                return <option key={res} value={res}>{res}</option>;
-                            })
-                        }
-                    </select>
-                  </td>
+  }, []);
+
+  const handleResponseChange = (questionId, selectedResponse) => {
+    setUserResponses((prevUserResponses) => ({
+      ...prevUserResponses,
+      [questionId]: selectedResponse,
+    }));
+  };
+
+  const handleSubmitResponses = () => {
+    // Prepare user responses for submission in the expected format
+    const userResponsesForApi = {};
+    for (const questionId in userResponses) {
+      userResponsesForApi[questionId] = userResponses[questionId];
+    }
+
+    // Send user responses to the server
+    axios.post("http://127.0.0.1:8000/api/diagnostics/batchStore", { responses: userResponses })
+      .then((response) => {
+        console.log("Responses submitted successfully", response);
+      })
+      .catch((error) => {
+        console.error("Error submitting responses:", error);
+      });
+  };
+
+  return (
+    <div>
+      <Dashboard lien1="questionnaire" lien2="diagnosticEntreprise" />
+      <div className="container mt-5">
+        <div className="row">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th colSpan="3" style={{ color: "white", fontSize: "30px", backgroundColor: "#033C4D" }}>
+                    Question table
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-        <button className="btn btn-primary" style={{width:"200px"}} type="submit" onClick={handleSubmitResponse}>Submit Responses</button>
+              </thead>
+              <thead>
+                <tr>
+                  <th style={{ width: "500px" }}>Category</th>
+                  <th style={{ width: "500px" }}>Question</th>
+                  <th>Response</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.category}</td>
+                    <td>{item.description}</td>
+                    <td>
+                      <select
+                        style={{ width: "500px" }}
+                        onChange={(e) => handleResponseChange(item.id, e.target.value)}
+                        value={userResponses[item.id] || ""}
+                      >
+                        <option value="">Select a response</option>
+                        {response.map((res) => (
+                          <option key={res.id} value={res.id}>
+                            {res.description}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+          <button className="btn btn-primary" style={{ width: "200px" }} onClick={handleSubmitResponses}>
+            Submit Responses
+          </button>
+        </div>
       </div>
-      </div>
-      </div>
-    )
+    </div>
+  );
 }
